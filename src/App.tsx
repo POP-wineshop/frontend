5bd290a51bafa8e0a380f97b38ea7462c69a3b4c
dilkusha27 <dilkusha27@gmail.com>
2025-04-24 14:56:26 +0900

    feat: 프로젝트 전역 react-router-dom 적용 및 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WineListPage from './pages/WineListPage';
+import WineDescriptionPage from './pages/WineDescriptionPage';
+import OrderPage from './pages/OrderPage';
+import CartPage from './pages/CartPage';
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Routes>
+        <Route path="/list" element={<WineListPage />} />
+        <Route path="/description" element={<WineDescriptionPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
