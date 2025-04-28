84f06927f874346f3e82ccfbb22208dba63fe6aa
dilkusha27 <dilkusha27@gmail.com>
2025-04-28 19:38:34 +0900

    chore: 전체 설정 적용 및 이미지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import OrderPage from './pages/OrderPage';
 import CartPage from './pages/CartPage';
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/common/Header';
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/list" element={<WineListPage />} />
-        <Route path="/description" element={<WineDescriptionPage />} />
-        <Route path="/order" element={<OrderPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
+      <div className="layout">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/list" element={<WineListPage />} />
+            <Route path="/description" element={<WineDescriptionPage />} />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
