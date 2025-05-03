e4cc145f22bfbab5b585f31e653764f9409ddd17
dilkusha27 <dilkusha27@gmail.com>
2025-05-03 23:30:45 +0900

    ui/ux: 와인 목록 페이지 및 컴포넌트 Tailwind CSS 적용

--- src/components/wineList/WineItem.tsx
+++ src/components/wineList/WineItem.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+import DuckhornMerlot from '@/assets/wineItem/Duckhorn_Napa Valley_Merlot.png';
+
 const WineItem = () => {
+  const [wineNameEng, setWineNameEng] = useState<string>('');
+  const [wineNameKor, setWineNameKor] = useState<string>('');
+
   return (
     <>
-      <div className="wine-item">
-        <div className="wine-name">
-          <p className="wine-name-kor">와인 이름 (한글)</p>
-          <p className="wine-name-eng">와인 이름 (영문)</p>
+      <div className="wine-item w-full flex flex-col items-center bg-white rounded-lg p-4 h-96">
+        <div className="w-full h-3/4 flex items-center justify-center overflow-hidden border ">
+          <img
+            src={DuckhornMerlot}
+            alt="와인 이미지"
+            className="object-contain w-full h-56"
+          />
         </div>
-        <p className="short-wine-description">간략한 와인 설명</p>
+
+        <div className="wine-name w-full text-left mt-4">
+          <p className="font-bold text-xl">와인 한글 이름</p>
+          <p className="text-lg text-gray-500">와인 영어 이름</p>
+          {/* <p className="font-bold text-2xl">{wineNameKor}</p>
+          <p className="text-lg text-gray-500">{wineNameEng}</p> */}
+        </div>
+
+        <div className="w-full mt-2 text-left text-base text-gray-600">
+          <p>간략한 와인 설명</p>
+        </div>
+
+        <div className="w-full mt-2 text-right text-base font-bold text-gray-600">
+          {/* <p>₩{price}</p> */}
+          <p>₩144,000</p>
+        </div>
       </div>
     </>
   );
--- src/pages/WineListPage.tsx
+++ src/pages/WineListPage.tsx
@@ -1,28 +1,44 @@
 import WineFilter from '../components/common/WineFilter';
 import WineItem from '../components/wineList/WineItem';
+import ToFirstPage from '@/assets/pagination/WineListPage_Pagination_ToFirstPage.svg';
+import ToPreviousPage from '@/assets/pagination/WineListPage_Pagination_ToPreviousPage.svg';
+import ToNextPage from '@/assets/pagination/WineListPage_Pagination_ToNextPage.svg';
+import ToLastPage from '@/assets/pagination/WineListPage_Pagination_ToLastPage.svg';
 
 const WineListPage = () => {
   return (
     <>
-      <WineFilter />
-      <div className="wine-items">
-        <WineItem />
-        <WineItem />
-        <WineItem />
-        <WineItem />
-        <WineItem />
-        <WineItem />
-        <WineItem />
-        <WineItem />
-      </div>
-      <div className="pagination">
-        <img src="" alt="이전 페이지" />
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>5</span>
-        <img src="" alt="다음 페이지" />
+      <div className="flex flex-col items-center ">
+        <WineFilter />
+        <div className="grid grid-cols-3 gap-4 w-full px-8">
+          <WineItem />
+          <WineItem />
+          <WineItem />
+          <WineItem />
+          <WineItem />
+          <WineItem />
+        </div>
+        <div className="flex items-center justify-center gap-4 my-8">
+          <img src={ToFirstPage} alt="첫 페이지" />
+          <img src={ToPreviousPage} alt="이전 페이지" />
+          <span>
+            <a href="">1</a>
+          </span>
+          <span>
+            <a href="">2</a>
+          </span>
+          <span>
+            <a href="">3</a>
+          </span>
+          <span>
+            <a href="">4</a>
+          </span>
+          <span>
+            <a href="">5</a>
+          </span>
+          <img src={ToNextPage} alt="다음 페이지" />
+          <img src={ToLastPage} alt="마지막 페이지" />
+        </div>
       </div>
     </>
   );
