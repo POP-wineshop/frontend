790fc8b154e4bf88e64d89fa5778924bf69a0003
dilkusha27 <dilkusha27@gmail.com>
2025-05-10 22:04:28 +0900

    ui/ux: 헤더 및 와인 검색 필터 컴포넌트 Tailwind CSS 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,20 @@
 const Header = () => {
   return (
-    <>
-      <div className="logo">
-        <img className="LogoImage" src="" alt="" />
-        <p className="LogoText">WINEHALLE</p>
+    <div className="flex items-center justify-between px-8 py-4 border-b">
+      <div className="flex items-center gap-4 ">
+        <img
+          className="w-16 h-16 rounded"
+          src="src/assets/logo/logo-Image.png"
+          alt="로고 이미지"
+        />
+        <strong className="text-2xl italic">WINEHALLE</strong>
       </div>
-      <div className="user-function">
-        <p className="my-page">Mypage</p>
-        <p className="likes">Likes</p>
-        <p className="cart">Cart</p>
+      <div className="flex items-center gap-8 text-lg">
+        <span className="hover:underline italic cursor-pointer">My page</span>
+        <span className="hover:underline italic cursor-pointer">Likes</span>
+        <span className="hover:underline italic cursor-pointer">Cart</span>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/common/WineFilter.tsx
+++ src/components/common/WineFilter.tsx
@@ -1,51 +1,77 @@
 const WineFilter = () => {
+  const nations = [
+    '생산국',
+    '프랑스',
+    '이탈리아',
+    '스페인',
+    '독일',
+    '미국',
+    '호주',
+    '뉴질랜드',
+    '칠레',
+    '아르헨티나',
+  ];
+
+  const regions = [
+    '생산지',
+    '꼬드 드 뉘 > 뉘 생 조르쥬',
+    '꼬뜨 드 뉘',
+    '꼬뜨 드 뉘 빌라쥐',
+    '꼬뜨 드 본',
+    '꼬뜨 드 본 > 뫼르소',
+    '꼬뜨 드 본 > 뽀마르',
+    '꼬뜨 드 본 > 상뜨네',
+    '꼬뜨 드 본 > 알록스 꼬르통',
+    '마꼬네',
+  ];
+
+  const wineTypes = [
+    '와인종류',
+    '레드',
+    '화이트',
+    '로제',
+    '스파클링',
+    '디저트',
+  ];
+
   return (
     <>
-      <div className="wine-filter-search">
-        <div className="wine-filter-nation">
-          <p>생산국</p>
-          <ul>
-            <li>생산국</li>
-            <li>프랑스</li>
-            <li>이탈리아</li>
-            <li>스페인</li>
-            <li>독일</li>
-            <li>미국</li>
-            <li>호주</li>
-            <li>뉴질랜드</li>
-            <li>칠레</li>
-            <li>아르헨티나</li>
-          </ul>
+      <div className="wine-filter-search flex justify-between items-center my-8 px-8 w-full min-w-[700px] mx-auto">
+        <div className="wine-filter flex gap-4 items-center">
+          <div className="wine-filter-nation items-center">
+            <select className="border w-32 p-2 rounded">
+              {nations.map((nation, index) => (
+                <option key={index}>{nation}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="wine-filter-region items-center">
+            <select className="border w-32 p-2 rounded">
+              {regions.map((region, index) => (
+                <option key={index}>{region}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="wine-filter-type items-center">
+            <select className="border w-32 p-2 rounded">
+              {wineTypes.map((type, index) => (
+                <option key={index}>{type}</option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="wine-filter-region">
-          <p>생산지</p>
-          <ul>
-            <li>생산지</li>
-            <li>부르고뉴 &gt; 꼬드 드 뉘 &gt; 뉘 생 조르쥬</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 뉘</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 뉘-빌라쥐</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 본</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 본 &gt; 뫼르소</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 본 &gt; 뽀마르</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 본 &gt; 상뜨네</li>
-            <li>부르고뉴 &gt; 꼬뜨 드 본 &gt; 알록스 꼬르통</li>
-            <li>부르고뉴 &gt; 마꼬네</li>
-          </ul>
-        </div>
-        <div className="wine-filter-type">
-          <p>와인종류</p>
-          <ul>
-            <li>와인종류</li>
-            <li>레드</li>
-            <li>화이트</li>
-            <li>로제</li>
-            <li>스파클링</li>
-            <li>디저트</li>
-          </ul>
-        </div>
-        <div className="wine-search">
-          <input type="text" placeholder="와인 이름" />
-          <button />
+
+        <div className="wine-search flex items-center justify-end">
+          <div className="flex items-center gap-2">
+            <input
+              type="text"
+              placeholder="와인 이름을 적어주세요!"
+              className="border w-64 p-2 rounded"
+            />
+            <button className="border p-2 rounded">검색</button>
+          </div>
         </div>
       </div>
     </>
