72145bea478f2a3c0437b0af9cf1469c6595a7b3
dilkusha27 <dilkusha27@gmail.com>
2025-05-15 22:51:02 +0900

    ui/ux: 백오피스 내용 중 와인 리스트 레이아웃 및 컴포넌트 작성

---
+++ src/components/backOffice/productList/ProductList.tsx
@@ -0,0 +1,21 @@
+import ProductItem from './ProductItem';
+
+const ProductList = () => {
+  return (
+    <>
+      <div className="productList-container flex flex-col">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </>
+  );
+};
+export default ProductList;
