39b8f799ee3b7e5c6218e550eac5bd4d74097a0f
dilkusha27 <dilkusha27@gmail.com>
2025-05-26 02:49:44 +0900

    chore: 외부 상수 모듈에 mock 생성 및 제품 리스트 내 개별 아이템에 적용

--- src/components/backOffice/product/ProductItem.tsx
+++ src/components/backOffice/product/ProductItem.tsx
@@ -1,4 +1,5 @@
 import { Product } from '@/types/backOffice/product/product';
+import { ArrowRight } from 'lucide-react';
 import { useState } from 'react';
 
 interface ProductItemProps {
@@ -10,11 +11,13 @@ const ProductItem = ({ product }: ProductItemProps) => {
 
   return (
     <>
-      <div className="productItem-container flex border rounded p-4 mb-4 shadow bg-grey-50 cursor-pointer">
+      <div className="productItem-container group relative flex border rounded p-4 mb-4 shadow bg-grey-50 cursor-pointer">
         <table className="w-full">
           <tbody>
             <tr>
-              <td colSpan={3}>{product.wineName.korean}</td>
+              <td className="font-semibold" colSpan={3}>
+                {product.wineName.korean}
+              </td>
               <td>가격: {product.price.value}</td>
             </tr>
             <tr>
@@ -23,21 +26,9 @@ const ProductItem = ({ product }: ProductItemProps) => {
               <td>빈티지: {product.vintage}</td>
               <td>재고: {product.stock}</td>
             </tr>
-            {/* <tr>
-              <td colSpan={3}>
-                <span className="font-semibold">덕혼 나파밸리 멜롯</span>
-              </td>
-              <td>가격: 120,000원</td>
-            </tr>
-            <tr>
-              <td>타입: 레드</td>
-              <td>국가: 미국</td>
-              <td>빈티지: 2021</td>
-              <td>재고: 10개</td>
-            </tr> */}
           </tbody>
         </table>
-        <img src="" alt="상세 아이콘" />
+        <ArrowRight className="absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400 opacity-0 group-hover:opacity-100 transition" />
       </div>
     </>
   );
