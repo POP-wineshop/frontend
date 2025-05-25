f21ba435703957d91d71dd86e92b07b3bed6dffc
dilkusha27 <dilkusha27@gmail.com>
2025-05-26 02:43:39 +0900

    ui/ux: 대외용 페이지와 백오피스 페이지 간 레이아웃 규격 다르게 만듬

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,15 @@ import WineApiTestPage from './pages/WineApiTestPage';
 import './App.css';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/common/Header';
-import BackOfficePage from './pages/BackOfficePage';
+import BackOfficePage from './pages/backOffice/BackOfficePage';
 import WineFilter from './components/common/WineFilter';
+import BackOfficeSidebar from './components/backOffice/sidebar/BackOfficeSidebar';
 
 function App() {
   const location = useLocation();
+  const shouldShowBackOffice = location.pathname.startsWith(`/backoffice`);
   const shouldShowHeader =
     !location.pathname.startsWith(`/apitest`) &&
-    !location.pathname.startsWith(`/backoffice`) &&
     !location.pathname.startsWith(`/post`);
   const shouldShowWineFilter =
     location.pathname.startsWith(`/list`) ||
@@ -25,23 +26,32 @@ function App() {
 
   return (
     <>
-      <div className="layout">
-        {shouldShowHeader ? <Header /> : null}
-        {shouldShowWineFilter ? <WineFilter /> : null}
-        <main>
+      {shouldShowBackOffice ? (
+        <div className="backOffice-layout">
           <Routes>
-            <Route path="/list" element={<WineListPage />} />
-            <Route path="/description/:id" element={<WineDescriptionPage />} />
-            <Route path="/order" element={<OrderPage />} />
-            <Route path="/cart" element={<CartPage />} />
-
-            <Route path="/post" element={<WinePostPage />} />
-            <Route path="/apitest" element={<WineApiTestPage />} />
-
-            <Route path="/backoffice" element={<BackOfficePage />} />
+            <Route path="/backoffice/*" element={<BackOfficePage />} />
           </Routes>
-        </main>
-      </div>
+        </div>
+      ) : (
+        <div className="main-layout">
+          {shouldShowHeader ? <Header /> : null}
+          {shouldShowWineFilter ? <WineFilter /> : null}
+          <main>
+            <Routes>
+              <Route path="/list" element={<WineListPage />} />
+              <Route
+                path="/description/:id"
+                element={<WineDescriptionPage />}
+              />
+              <Route path="/order" element={<OrderPage />} />
+              <Route path="/cart" element={<CartPage />} />
+
+              <Route path="/post" element={<WinePostPage />} />
+              <Route path="/apitest" element={<WineApiTestPage />} />
+            </Routes>
+          </main>
+        </div>
+      )}
     </>
   );
 }
