2101926390c0936704ddbf4d5fdcbae6fbd3a0c0
dilkusha27 <dilkusha27@gmail.com>
2025-06-02 22:41:26 +0900

    feat: 와인 검색 필터 기능 활성화

--- src/components/common/WineFilter.tsx
+++ src/components/common/WineFilter.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
+
 const WineFilter = () => {
-  const nations = [
+  const countries = [
     '생산국',
     '프랑스',
     '이탈리아',
@@ -26,38 +28,85 @@ const WineFilter = () => {
   ];
 
   const wineTypes = [
-    '와인종류',
-    '레드',
-    '화이트',
-    '로제',
-    '스파클링',
-    '디저트',
+    { key: '와인종류', value: '' },
+    { key: '레드', value: 'RED' },
+    { key: '화이트', value: 'WHITE' },
+    { key: '로제', value: 'ROSE' },
+    { key: '스파클링', value: 'SPARKLING' },
+    { key: '디저트', value: 'DESSERT' },
   ];
+
+  const [country, setCountry] = useState<string>('');
+  const [region, setRegion] = useState<string>('');
+  const [wineType, setWineType] = useState<string>('');
+  const [keyword, setKeyword] = useState<string>('');
+
+  const handleSearchWines = () => {
+    const url = new URL('http://localhost:8080/api/wines/search');
+    const params = new URLSearchParams(url.search);
+
+    if (country) params.append('country', country);
+    if (region) params.append('region', region);
+    if (wineType) params.append('wineType', wineType);
+    if (keyword) params.append('keyword', keyword);
+
+    const finalUrl = `${url.origin}${url.pathname}?${params.toString()}`;
+
+    fetch(finalUrl)
+      .then((res) => res.json())
+      .then((jsonRes) => {
+        console.log(jsonRes);
+        alert(`와인 조회 성공! 
+          url : ${finalUrl}
+          국가 / 지역 : ${country} > ${region}
+          종류 : ${wineType}
+          키워드 : ${keyword}`);
+        // 전역 변수로 와인 목록 컴포넌트에 들어갈 와인 데이터 상태 변경
+      })
+      .catch((error) =>
+        alert(`와인 조회 실패 ㅠ
+          국가 / 지역 : ${country} > ${region}
+          종류 : ${wineType}
+          키워드 : ${keyword}
+          에러: ${error}`)
+      );
+  };
 
   return (
     <>
       <div className="wine-filter-search flex justify-between items-center my-8 px-8 w-full min-w-[700px] mx-auto">
         <div className="wine-filter flex gap-4 items-center">
-          <div className="wine-filter-nation items-center">
-            <select className="border w-32 p-2 rounded">
-              {nations.map((nation, index) => (
-                <option key={index}>{nation}</option>
+          <div className="wine-filter-country items-center">
+            <select
+              onChange={(e) => setCountry(e.target.value)}
+              className="border w-32 p-2 rounded"
+            >
+              {countries.map((country) => (
+                <option key={country}>{country}</option>
               ))}
             </select>
           </div>
 
           <div className="wine-filter-region items-center">
-            <select className="border w-32 p-2 rounded">
-              {regions.map((region, index) => (
-                <option key={index}>{region}</option>
+            <select
+              onChange={(e) => setRegion(e.target.value)}
+              className="border w-32 p-2 rounded"
+            >
+              {regions.map((region) => (
+                <option key={region}>{region}</option>
               ))}
             </select>
           </div>
 
-          <div className="wine-filter-type items-center">
-            <select className="border w-32 p-2 rounded">
-              {wineTypes.map((type, index) => (
-                <option key={index}>{type}</option>
+          <div className="wine-filter-wineType items-center">
+            <select
+              onChange={(e) => setWineType(e.target.value)}
+              className="border w-32 p-2 rounded"
+            >
+              {wineTypes.map((wineType) => (
+                <option key={wineType.key} value={wineType.value}>
+                  {wineType.key}
+                </option>
               ))}
             </select>
           </div>
@@ -69,8 +118,11 @@ const WineFilter = () => {
               type="text"
               placeholder="와인 이름을 적어주세요!"
               className="border w-64 p-2 rounded"
+              onChange={(e) => setKeyword(e.target.value)}
             />
-            <button className="border p-2 rounded">검색</button>
+            <button onClick={handleSearchWines} className="border p-2 rounded">
+              검색
+            </button>
           </div>
         </div>
       </div>
