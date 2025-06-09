39e90cbebd6c7b97262cfc83c30037e559ca64f8
dilkusha27 <dilkusha27@gmail.com>
2025-06-09 23:40:14 +0900

    feat: 주문에 반영될 아이템 전체 선택, 아이템 삭제에 필요한 개별 선택 적용

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import CartItem from '../components/cart/CartItem';
 import CartPayment from '../components/cart/CartPayment';
+import { useNavigate } from 'react-router-dom';
 
 type CartWineItem = {
   cartItemId: number;
@@ -11,13 +12,16 @@ type CartWineItem = {
 };
 
 type CartItemList = CartWineItem[];
-
-type SelectedCartItemList = CartWineItem[];
+type SelectedItemList = CartWineItem[];
 
 const CartPage = () => {
-  const [cartItemList, setCartItemList] = useState<CartItemList | null>();
+  const navigate = useNavigate();
+
+  const [cartItemList, setCartItemList] = useState<CartItemList>([]);
   const [selectedCartItemList, setSelectedCartItemList] =
-    useState<SelectedCartItemList | null>();
+    useState<SelectedItemList>([]);
+  const [allCartItemsSelected, setAllCartItemsSelected] =
+    useState<boolean>(false);
 
   // 장바구니 아이템 목록 조회
   useEffect(() => {
@@ -28,16 +32,20 @@ const CartPage = () => {
     })
       .then((res) => res.json())
       .then((jsonRes) => {
-        // data가 빈 배열, 혹은 빈 객체일 때
-        if (Array.isArray(jsonRes.data) && jsonRes.data.length === 0) {
+        // data가 객체이면 배열로 변환
+        const dataList = Array.isArray(jsonRes.data)
+          ? jsonRes.data
+          : [jsonRes.data];
+
+        if (dataList.length === 0) {
           console.warn('빈 주문 응답 수신됨');
           alert('주문 항목이 비어 있음. 다시 시도하세요.');
           return;
         }
 
-        setCartItemList(jsonRes.data);
-        console.log(`장바구니 조회 성공 : `, jsonRes.data);
-        alert(`장바구니 조회 성공! : ${jsonRes.data}`);
+        setCartItemList(dataList);
+        console.log(`장바구니 조회 성공 : `, dataList);
+        alert(`장바구니 조회 성공! : ${dataList}`);
       })
       .catch((err) => {
         console.error(`장바구니 조회 실패 : `, err);
@@ -45,7 +53,38 @@ const CartPage = () => {
       });
   }, []);
 
-  // 선택된 장바구니 아이템 삭제
+  // 아이템 전체 선택
+  const handleSelectAllCartItems = () => {
+    if (allCartItemsSelected) {
+      setSelectedCartItemList([]);
+      setAllCartItemsSelected(false);
+    } else {
+      setSelectedCartItemList(cartItemList);
+      setAllCartItemsSelected(true);
+    }
+  };
+
+  // 아이템 개별 선택
+  const handleSelectCartItem = (id: number) => {
+    if (selectedCartItemList.some((item) => item.cartItemId === id)) {
+      const updatedList = selectedCartItemList.filter(
+        (cartItem) => cartItem.cartItemId !== id
+      );
+      setSelectedCartItemList(updatedList);
+    } else {
+      const targetItem = cartItemList.find(
+        (cartItem) => cartItem.cartItemId === id || ''
+      );
+
+      if (targetItem) {
+        // targetItem이 없을 경우에 대한 방지
+        const updatedList = [...(selectedCartItemList || []), targetItem];
+        setSelectedCartItemList(updatedList);
+      }
+    }
+  };
+
+  // 선택된 장바구니 아이템 삭제 => [Refactor 필요] 반복 호출이 아닌 여러 item을 한번에 처리하는 방향으로
   const handleDeleteSelectedCartItems = () => {
     if (selectedCartItemList) {
       selectedCartItemList.forEach((item) => {
@@ -81,16 +120,41 @@ const CartPage = () => {
         body: JSON.stringify(selectedCartItemList),
       })
         .then((res) => res.json())
-        .then(() => {
+        .then((jsonRes) => {
           console.log(
             `장바구니 내 선택된 아이템 주문 생성 성공 : `,
             item.cartItemId
           );
           alert(`장바구니 내 선택된 아이템 주문 생성 성공!`);
+          navigate(`/order`, { state: { orderId: jsonRes.data.orderId } });
         })
         .catch((error) => {
           console.error(`장바구니 내 선택된 아이템 주문 생성 실패 : `, error);
           alert(`장바구니 내 선택된 아이템 주문 생성 실패 ㅠ : ${error}`);
+        });
+    });
+  };
+
+  // 모든 장바구니 아이템 주문 생성
+  const handleOrderAllCartItems = () => {
+    cartItemList.forEach((item) => {
+      fetch(`http://localhost:8080/api/orders/from-cart`, {
+        method: 'POST',
+        headers: {
+          Authorization: `${localStorage.getItem('Access Token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cartItemList),
+      })
+        .then((res) => res.json())
+        .then((jsonRes) => {
+          console.log(`장바구니 내 모든 아이템 주문 생성 성공 : `, jsonRes);
+          alert(`장바구니 내 모든 아이템 주문 생성 성공!`);
+          navigate(`/order`, { state: { orderId: jsonRes.data.orderId } });
+        })
+        .catch((error) => {
+          console.error(`장바구니 내 모든 아이템 주문 생성 실패 : `, error);
+          alert(`장바구니 내 모든 아이템 주문 생성 실패 ㅠ : ${error}`);
         });
     });
   };
@@ -105,22 +169,41 @@ const CartPage = () => {
         </div>
         <div className="cart-items-container w-full py-8">
           <div className="flex gap-4 my-2">
-            <input type="checkbox" />
-            <span className="text-xs font-bold">전체 선택</span>
+            <input
+              type="checkbox"
+              checked={allCartItemsSelected}
+              onChange={handleSelectAllCartItems}
+            />
+            <span
+              className="text-xs font-bold hover:underline cursor-pointer"
+              onClick={handleSelectAllCartItems}
+            >
+              전체 선택
+            </span>
             <span
               onClick={handleDeleteSelectedCartItems}
-              className="text-xs font-bold"
+              className="text-xs font-bold hover:underline cursor-pointer"
             >
               선택 삭제
             </span>
           </div>
           {/* 주문 진행 중인 상품 목록의 데이터 형태에 따라 달라짐 */}
           {cartItemList?.map((cartItem) => (
-            <CartItem key={cartItem.cartItemId} cartWineItem={cartItem} />
+            <CartItem
+              key={cartItem.cartItemId}
+              cartWineItem={cartItem}
+              selected={selectedCartItemList.some(
+                (item) => item.cartItemId === cartItem.cartItemId
+              )}
+              onSelect={() => handleSelectCartItem(cartItem.cartItemId)}
+            />
           ))}
         </div>
         <div className="cart-payment-container w-full py-8">
-          <CartPayment />
+          <CartPayment
+            onOrderSelected={handleOrderSelectedCartItems}
+            onOrderAll={handleOrderAllCartItems}
+          />
         </div>
       </div>
     </div>
