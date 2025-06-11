fab1f30a235794ad6c1af92c94403e6cb71140cd
dilkusha27 <dilkusha27@gmail.com>
2025-06-11 23:46:50 +0900

    feat: CartPage에서 props로 받은 장바구니 내 제품 수량 상태 증가 / 감소 로직 반영, 개별 즉시 주문 기능 구현

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -2,65 +2,76 @@ import { Heart, HeartPlus } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import DuckhornMerlot from '@/assets/wineItem/Duckhorn_Napa Valley_Merlot.png';
 import { useNavigate } from 'react-router-dom';
+import { substractQuantity } from '@/utils/common/util';
 
 type CartWineItem = {
   cartItemId: number;
   wineName: string;
   quantity: number;
   totalPrice: number;
+  wineId: number;
   thumbnail: string;
+  winePrice: number;
 };
 
 type CartItemProps = {
   cartWineItem: CartWineItem;
   selected: boolean;
   onSelect: () => void;
+  onAddQuantityState: (id: number) => void;
+  onSubstractQuantityState: (id: number) => void;
 };
 
 export const CartItem = ({
   cartWineItem,
   selected,
   onSelect,
+  onAddQuantityState,
+  onSubstractQuantityState,
 }: CartItemProps) => {
   const navigate = useNavigate();
 
   // cartItemId가 wineId가 아닌가보다. 데이터 내에 wineId를 같이 보내줘야함
 
-  const [cartItemId, setCartItemId] = useState<number>(0);
+  // const [cartItemId, setCartItemId] = useState<number>(0);
+  const [wineId, setWineId] = useState<number>(0);
   const [wineNameEng, setWineNameEng] = useState<string>('와인 영어 이름');
   const [wineNameKor, setWineNameKor] = useState<string>('');
   const [cartItemQuantity, setCartItemQuantity] = useState<number>(0);
   // const [itemPrice, setItemPrice] = useState<number>(0);
-  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [cartItemPrice, setCartItemPrice] = useState<number>(0);
   const [deliveryFee, setDeliveryFee] = useState<number>(0);
-
   // const [isSelected, setIsSelected] = useState<boolean>(false);
   const [isLiked, setIsLiked] = useState<boolean>(false);
 
   useEffect(() => {
     if (!cartWineItem) return;
 
-    setCartItemId(cartWineItem.cartItemId);
+    // setCartItemId(cartWineItem.cartItemId);
+    setWineId(cartWineItem.wineId);
     setWineNameKor(cartWineItem.wineName);
     setCartItemQuantity(cartWineItem.quantity);
+    console.log(
+      `wineNameKor: ${wineNameKor}, CartItemQuantity: , ${cartItemQuantity}`
+    );
     // setTotalPrice(cartWineItem.totalPrice);
-    setTotalPrice(cartWineItem.totalPrice); // 바뀔 걸 생각하면 여기도 개별 가격을 받아왔어야 하네;;;
+    setCartItemPrice(cartWineItem.winePrice); // 바뀔 걸 생각하면 여기도 개별 가격을 받아왔어야 하네;;;
     // setDeliveryFee(cartWineItem.totalPrice >= 50000 ? 0 : 3000); // 예시: 5만원 이상 무료배송
     setIsLiked(false); // 초기 좋아요는 false로 세팅 (필요 시 API 호출)
   }, [cartWineItem]);
 
-  const substractQuantity = () => {
-    if (cartItemQuantity >= 2) {
-      return setCartItemQuantity(cartItemQuantity - 1);
-    } else {
-      alert('1 이하로는 수량을 줄일 수 없습니다.');
-      return null;
-    }
-  };
+  // const substractQuantity = () => {
+  //   if (cartItemQuantity >= 2) {
+  //     return setCartItemQuantity(cartItemQuantity - 1);
+  //   } else {
+  //     alert('1 이하로는 수량을 줄일 수 없습니다.');
+  //     return null;
+  //   }
+  // };
 
-  const addQuantity = () => {
-    return setCartItemQuantity(cartItemQuantity + 1);
-  };
+  // const addQuantity = () => {
+  //   return setCartItemQuantity(cartItemQuantity + 1);
+  // };
 
   // 숫자를 회계단위로 변환
   // => toLocaleString()으로 해결 가능
@@ -69,10 +80,11 @@ export const CartItem = ({
   // }
 
   const instantOrderData = {
-    wineId: cartItemId,
+    wineId: wineId,
     quantity: cartItemQuantity,
   };
 
+  // 상품 개별 주문 페이지로 이동
   const handleInstantOrder = () => {
     fetch(`http://localhost:8080/api/orders/instant`, {
       method: 'POST',
@@ -114,8 +126,6 @@ export const CartItem = ({
         <div className="cart-item-context h-60 m-3 w-full">
           <div className="flex justify-between items-center h-1/2">
             <div className="flex flex-col justify-center items-left ">
-              {/* <p className="cart-item-name-eng">{wineNameEng}</p>
-              <p className="cart-item-name-kor">{wineNameKor}</p> */}
               <span className="cart-item-name-eng text-lg font-base">
                 {wineNameEng}
               </span>
@@ -144,11 +154,23 @@ export const CartItem = ({
           </div>
           <div className="flex justify-between items-end h-1/3">
             <div className="cart-item-quantity-control flex items-center">
-              <button className="border w-6" onClick={substractQuantity}>
+              <button
+                className="border w-6"
+                onClick={() => {
+                  onSubstractQuantityState(wineId);
+                  console.log('수량 빼기 버튼 클릭');
+                }}
+              >
                 -
               </button>
               <span className="w-12 text-center">{cartItemQuantity}</span>
-              <button className="border w-6" onClick={addQuantity}>
+              <button
+                className="border w-6"
+                onClick={() => {
+                  onAddQuantityState(wineId);
+                  console.log('수량 더하기 버튼 클릭');
+                }}
+              >
                 +
               </button>
             </div>
@@ -158,8 +180,7 @@ export const CartItem = ({
                 개별배송
               </p>
               <span className="cart-item-cost text-right text-2xl font-bold">
-                {/* ₩{(cartItemQuantity * cartItemPrice).toLocaleString()} */}₩
-                {totalPrice.toLocaleString()}
+                ₩{(cartItemQuantity * cartItemPrice).toLocaleString()}
               </span>
             </div>
           </div>
